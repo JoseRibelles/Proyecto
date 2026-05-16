c6c3ef01288374a5f4a77f132058b0a101871e24
JoseRibelles <joseantonioribellesgarcia@elpuig.xeill.net>
2026-05-17 01:12:39 +0200

    Add --force-yes flag to Strapi import command

--- backend/import.js
+++ backend/import.js
@@ -1,12 +1,10 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
-
 const exportFile = "./data-import/export.tar.gz.tar.gz.enc";
-
 if (fs.existsSync(exportFile)) {
   try {
     console.log("Importando datos...");
-    execSync(`pnpm strapi import --file ${exportFile} --key Millet2024!`, { stdio: "inherit" });
+    execSync(`pnpm strapi import --file ${exportFile} --key Millet2024! --force-yes`, { stdio: "inherit" });
     console.log("Importación completada.");
   } catch (error) {
     console.log("Error al importar:", error.message);
