dd452570b49634a08040b40037946b21edba76dc
JoseRibelles <joseantonioribellesgarcia@elpuig.xeill.net>
2026-05-17 00:38:29 +0200

    Update import.js

--- backend/import.js
+++ backend/import.js
@@ -1,12 +1,12 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
 
-const exportFile = "./data-import/export.tar.gz";
+const exportFile = "./data-import/export.tar.gz.tar.gz.enc";
 
 if (fs.existsSync(exportFile)) {
   try {
     console.log("Importando datos...");
-    execSync(`pnpm strapi import --file ${exportFile} --force`, { stdio: "inherit" });
+    execSync(`pnpm strapi import --file ${exportFile} --key Millet2024!`, { stdio: "inherit" });
     console.log("Importación completada.");
   } catch (error) {
     console.log("Error al importar:", error.message);
